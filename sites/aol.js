8cadbea4e5621dfeee18f2a6c17a22f93aa0971e
brad@danga.com <brad@danga.com@902e2b25-573d-0410-8f12-e1b9ea7b2021>
2008-04-17 23:18:52 +0000

    support newer http://profiles.aim.com/ URLs, making them now canonical as well
    git-svn-id: http://google-sgnodemapper.googlecode.com/svn/trunk@92 902e2b25-573d-0410-8f12-e1b9ea7b2021

--- sites/aol.js
+++ sites/aol.js
@@ -21,19 +21,19 @@
  *
  * @type RegExp
  */
-var AIM_REGEX = /^aim:(?:goim\?screenname=)?([\w \+]+)/i;
+var AIM_REGEX = /^aim:(?:goim\?screenname=)?([%\w \+]+)$/i;
 
 
 nodemapper.registerNonHTTPHandler(function(url) {
   var m = AIM_REGEX.exec(url);
   if (m) {
-    var screenname = m[1].toLowerCase().replace(/[\s\+]/g, "");
+    var screenname = m[1].toLowerCase().replace(/(?:[\s\+]|%20)/g, "");
     return "sgn://aol.com/?ident=" + screenname;
   }
 });
 
 
-nodemapper.registerDomain("openid.aol.com", {
+nodemapper.registerDomain(["openid.aol.com", "profiles.aim.com"], {
  urlToGraphNode: nodemapper.createSlashUsernameHandler("aol.com"),
 });
 
@@ -60,7 +60,7 @@ nodemapper.registerDomain(
  ident_to_openid: function (ident) { return "http://openid.aol.com/" + ident; },
  ident_to_chat: function (ident) { return "aim:GoIM?screenname=" + ident; },
  ident_to_profile: function (ident) {
-   return "http://www.aimpages.com/" + ident + "/profile.html";
+   return "http://profiles.aim.com/" + ident;
  }
 });
 
@@ -68,17 +68,18 @@ nodemapper.registerDomain(
 // But then how can I specify these custom profile/atom handlers?
 nodemapper.registerDomain("pictures.aol.com",
   {name: "AOL Pictures",
-   urlToGraphNode: nodemapper.createSomethingSlashUsernameHandler("galleries", 
+   urlToGraphNode: nodemapper.createSomethingSlashUsernameHandler("galleries",
       "pictures.aol.com")});
-nodemapper.addSimpleHandler("pictures.aol.com", "ident_to_profile", 
+nodemapper.addSimpleHandler("pictures.aol.com", "ident_to_profile",
       "http://pictures.aol.com/galleries/");
-nodemapper.addSimpleHandler("pictures.aol.com", "ident_to_atom", 
+nodemapper.addSimpleHandler("pictures.aol.com", "ident_to_atom",
       "http://pictures.aol.com/galleries/", "/atom.xml");
 
 __END__
 
 aim:GoIM?screenname=fooBar			sgn://aol.com/?ident=foobar
 aim:goim?Screenname=foo+Bar			sgn://aol.com/?ident=foobar
+aim:goim?screenname=foo%20bar                   sgn://aol.com/?ident=foobar
 aim:fooBar					sgn://aol.com/?ident=foobar
 aim:foo+Bar					sgn://aol.com/?ident=foobar
 http://www.aimpages.com/foobar/profile.html	sgn://aol.com/?ident=foobar
@@ -89,9 +90,11 @@ http://www.aimpages.com/foobar		sgn://aol.com/?ident=foobar
 http://openid.aol.com/foobar			sgn://aol.com/?ident=foobar
 http://openid.aol.com/foobar/		sgn://aol.com/?ident=foobar
 
+http://profiles.aim.com/foobar          sgn://aol.com/?ident=foobar
+
 openid(sgn://aol.com/?ident=foobar)		http://openid.aol.com/foobar
 chat(sgn://aol.com/?ident=foobar)		aim:GoIM?screenname=foobar
-profile(sgn://aol.com/?ident=foobar)		http://www.aimpages.com/foobar/profile.html
+profile(sgn://aol.com/?ident=foobar)		http://profiles.aim.com/foobar
 
 http://pictures.aol.com/galleries/josephsmarr sgn://pictures.aol.com/?ident=josephsmarr
 profile(sgn://pictures.aol.com/?ident=josephsmarr) http://pictures.aol.com/galleries/josephsmarr
