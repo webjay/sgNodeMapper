86d8ce650e3902c784a8e0093860fd97ff802163
brad@danga.com <brad@danga.com@902e2b25-573d-0410-8f12-e1b9ea7b2021>
2008-06-23 21:47:00 +0000

    Add web-based demo for google-sgnodemapper.
    git-svn-id: http://google-sgnodemapper.googlecode.com/svn/trunk@112 902e2b25-573d-0410-8f12-e1b9ea7b2021

---
+++ webdemo/demo.js
@@ -0,0 +1,119 @@
+function $(name) {
+  return document.getElementById(name);
+}
+
+function setStatus(text) {
+  $("status").innerHTML += "<div>" + text + "</div>";
+}
+
+function XHR() {
+  if (typeof(XMLHttpRequest) != "undefined") {
+    return new XMLHttpRequest();
+  }
+  var xhr;
+  var ex;
+  try {
+    xhr = new ActiveXObject("Msxml2.XMLHTTP.4.0");
+  } catch (ex) {
+    try {
+      xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (ex) {
+    }
+  }
+  return xhr;
+}
+
+function loadNodeMapper() {
+  setStatus("Loading...");
+  var request = XHR();
+  var sites_to_load = 0;
+  request.onreadystatechange = function() {
+    if (request.readyState != 4) return;
+    if (request.status != 200) {
+      setStatus("Error: " + request.responseText);
+      return;
+    }
+    setStatus("Parsing base rules.");
+    try {
+      eval(request.responseText);
+    } catch (error) {
+      setStatus("Error parsing nodemapper-base.js: " + error);
+      return;
+    }
+    setStatus("Parsed base.");
+    getSiteList(function (sites) {
+      sites_to_load = sites.length;
+      setStatus("Sites to load: " + sites_to_load);
+      var onSiteLoaded = function(site) {
+        sites_to_load--;
+        setStatus("Sites to load: " + sites_to_load);
+        if (sites_to_load == 0) {
+          $("status").innerHTML = "";
+          setStatus("Done loading <a href='../nodemapper-base.js'>nodemapper-base.js</a> & " + sites.length + " <a href='../sites/'>site rule files</a>.");
+        }
+      };
+      // kick off the loads of each site rule files
+      for (var i = 0; i < sites.length; ++i) {
+        startLoadingSiteRules(sites[i], onSiteLoaded);
+      }
+    });
+  };
+  request.open("GET", "../nodemapper-base.js", true);
+  request.send(null);
+}
+
+function startLoadingSiteRules(site, callback) {
+  var request = XHR();
+  request.onreadystatechange = function() {
+    if (request.readyState != 4) return;
+    if (request.status != 200) {
+      alert("Error loading " + site + ": " + request.responseText);
+    }
+    // need to split the __END__ block (of tests) away
+    setStatus("Size = " + request.responseText.length);
+    var splits = request.responseText.split("__END__");
+    var jsCode = splits[0];
+    var testData = splits[1];
+    try {
+      eval(jsCode);
+      setStatus("Tests for " + site + ": " + testData);
+      callback();
+    } catch (error) {
+      setStatus("Parsing " + site + " failed: " + error);
+    }
+  };
+  request.open("GET", "../sites/" + site, true);
+  request.send(null);
+}
+
+function getSiteList(callback) {
+  setStatus("Loading site list...");
+  var request = XHR();
+  request.onreadystatechange = function() {
+    if (request.readyState != 4) return;
+    if (request.status != 200) {
+      setStatus("Error getting /sites/: " + request.responseText);
+      return;
+    }
+    var sites = []
+    var siteRE = /a href="([\w-]+\.js)"/ig;
+    var matches;
+    while ((matches = siteRE.exec(request.responseText)) != null) {
+      sites.push(matches[1]);
+    }
+    callback(sites);
+  };
+  request.open("GET", "../sites/", true);
+  request.send(null);
+}
+
+function convertToSgn(evt) {
+  var input = $("tosgn").value;
+  var output = nodemapper.urlToGraphNode(input);
+  if (output == input) {
+    $("tosgn_output").innerHTML = "No known conversion for " + output;
+    return false;
+  }
+  $("tosgn_output").innerHTML = "Converted to: <b>" + output + "</b> (from " + input + ")";
+  return false;
+}
