ab10e1d69e3015ae1f182f289f033a28e0b3d61a
brad@danga.com <brad@danga.com@902e2b25-573d-0410-8f12-e1b9ea7b2021>
2008-06-25 15:43:31 +0000

    run and pass all tests in a browser.
    git-svn-id: http://google-sgnodemapper.googlecode.com/svn/trunk@122 902e2b25-573d-0410-8f12-e1b9ea7b2021

--- webdemo/runtests.js
+++ webdemo/runtests.js
@@ -9,9 +9,53 @@ function runNodeMapperTests(divName) {
         return;
     }
     var testCount = nodemapper_tests.length;
+    var testsRun = 0;
+    var testsPassed = 0;
+    var testFailures = "";
+
+    var updateStatus = function() {
+        div.innerHTML = testsRun + "/" + testCount + " tests run, " + testsPassed +
+            " passed.<br>" + testFailures;
+    };
+    var onPass = function() {
+        testsRun++;
+        testsPassed++;
+        updateStatus();
+    };
+    var onFail = function(test, actual) {
+        testsRun++;
+        testFailures += "<b>failure: </b> for " + test + ", got: "
+        + "<font color='red'>" + actual + "</font><br>";
+        updateStatus();
+    };
+    var onResult = function(test, actual) {
+        var expected = test[test.length - 1];
+        if (actual == expected) {
+            onPass();
+        } else {
+            onFail(test, actual);
+        }
+    };
+
     for (var i = 0; i < testCount; ++i) {
         var test = nodemapper_tests[i];
-        div.innerHTML += "Test: " + test + "<br>";
+        if (test[0] == "to_sgn") {
+            var actual = nodemapper.urlToGraphNode(test[1]);
+            onResult(test, actual);
+            continue;
+        }
+        if (test[0] == "from_sgn") {
+            var actual = nodemapper.urlFromGraphNode(test[2], test[1]);
+            onResult(test, actual);
+            // TODO: test round-tripping as well.
+            continue;
+        }
+        if (test[0] == "pair") {
+            var actual = nodemapper.pairToGraphNode(test[1], test[2]);
+            onResult(test, actual);
+            continue;
+        }
+
     }
-    div.innerHTML += "<b>Done running tests.</b>";
+
 }
