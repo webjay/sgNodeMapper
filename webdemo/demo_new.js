15212c2e10391474447e4f4297b2be5ae9a25a43
aidamina <aidamina@902e2b25-573d-0410-8f12-e1b9ea7b2021>
2009-02-03 18:43:52 +0000

    demo_new.html: Complete cleanup to pass http://validator.w3.org/ Added <h1>Demo for google-sgnodemapper</h1> Added sections for new features:   Settings -> Debug mode   Settings -> Permalink
    demo_new.js:
    New features:
      Permalink
      Debug/fast mode
      Effects
    
    Querystring arguments:
      q: escaped url (http) (defaults to empty)
      sgn: escaped url (sgn) (defaults to empty)
      debug: 1 for debug mode 0 for fast (defaults to 0)
      effects: pretty show hide speed (slow, normal, fast, or milisecond input) (defaults to 0)
    
    Example urls:
    http://google-sgnodemapper.googlecode.com/svn/trunk/webdemo/demo_new.html?effects=normal&q=http%3A%2F%2Fbrad.livejournal.com
    http://localhost/svg/webdemo/demo_new.html?debug=1&sgn=sgn%3A%2F%2Flivejournal.com%2F%3Fident%3Dbrad
    http://google-sgnodemapper.googlecode.com/svn/trunk/webdemo/demo_new.html?effects=slow
    http://google-sgnodemapper.googlecode.com/svn/trunk/webdemo/demo_new.html?effects=100
    
    git-svn-id: http://google-sgnodemapper.googlecode.com/svn/trunk@165 902e2b25-573d-0410-8f12-e1b9ea7b2021

--- webdemo/demo_new.js
+++ webdemo/demo_new.js
@@ -1,52 +1,147 @@
 // Use Google's  AJAX Libraries API to load jQuery only required for this demo)
 // We use jQuery (http://jquery.com) for the XMLHttpRequest calling
 // and the DOM manipulation.
+var useDebug = false;
+var effects = -1;
+var queryString;
 google.load("jquery", "1");
 google.setOnLoadCallback(function() {
-  setStatus("Loading nodemapper-base");
-  $.getScript("../nodemapper-base.js", function() {
-    setStatus("nodemapper-base loaded.");
-    $.get("../sites/", function(data) {
-      var sites = findSites(data);
-      var sites_to_load = sites.length;
-      setStatus("Loading rules for " + sites_to_load + " sites");
-      var onSiteLoaded = function() {
-        sites_to_load--;
-        setStatus("Sites to load: " + sites_to_load);
-        if (sites_to_load == 0) {
-          $("#status").empty();
-          setStatus("Done loading <a href='../nodemapper-base.js'>" +
-              "nodemapper-base.js</a> & " + sites.length +
-              " <a href='../sites/'>site rule files</a>.");
-        }
-      };
-      $.each(sites, function() {
-        var site = this;
-        $.get("../sites/" + site, function(data) {
-          var splits = data.split("__END__");
-          var jsCode = splits[0];
-          var testData = splits[1];
-          try {
-            eval(jsCode);
-            setStatus("Loaded " + site + ".");
-            onSiteLoaded();
-          } catch (error) {
-            if (error instanceof Error) {
-              var more = "";
-              for (var k in error) {
-                more += " [" + k + " == " + error[k] + "] ";
-              }
-              setStatus("Parsing " + site + " failed: " + more +
-                  " on content: " + "<pre>" + jsCode + "</pre>");
-            } else {
-              setStatus("Parsing " + site + " failed: " + error);
-            }
+  queryString = getQueryString();
+  useDebug = queryString.debug == "1";
+  $("#settings_debug").click(function() {
+    toggleDebug();
+  });
+  toggleDebug(useDebug);
+  if (queryString.effects) {
+    effects = isNaN(queryString.effects) ? queryString.effects :
+        parseInt(queryString.effects);
+    if(effects == 0)
+      effects--;
+  }
+  if (queryString.q) {
+    $("#tosgn_input").val(unescape(queryString.q));
+  }
+  if (queryString.sgn) {
+    $("#fromsgn_input").val(unescape(queryString.sgn));
+  }
+  if (useDebug) {
+    setStatus("Loading nodemapper-base");
+    $.getScript("../nodemapper-base.js", function() {
+      setStatus("nodemapper-base loaded.");
+      $.get("../sites/", function(data) {
+        var sites = findSites(data);
+        var sites_to_load = sites.length;
+        setStatus("Loading rules for " + sites_to_load + " sites");
+        var onSiteLoaded = function() {
+          sites_to_load--;
+          setStatus("Sites to load: " + sites_to_load);
+          if (sites_to_load == 0) {
+            $("#status").empty();
+            setStatus("Done loading <a href=\"../nodemapper-base.js\">" +
+                "nodemapper-base.js</a> & " + sites.length +
+                " <a href=\"../sites/\">site rule files</a>.");
+            nodemapperReady();
           }
+        };
+        $.each(sites, function() {
+          var site = this;
+          $.get("../sites/" + site, function(data) {
+            var splits = data.split("__END__");
+            var jsCode = splits[0];
+            var testData = splits[1];
+            try {
+              eval(jsCode);
+              setStatus("Loaded " + site + ".");
+              onSiteLoaded();
+            } catch (error) {
+              if (error instanceof Error) {
+                var more = "";
+                for (var k in error) {
+                  more += " [" + k + " == " + error[k] + "] ";
+                }
+                setStatus("Parsing " + site + " failed: " + more +
+                    " on content: " + "<pre>" + jsCode + "</pre>");
+              } else {
+                setStatus("Parsing " + site + " failed: " + error);
+              }
+            }
+          });
         });
       });
     });
-  });
+  } else {
+    setStatus("Loading nodemapper");
+    $.getScript("../autogen/nodemapper.js", function() {
+      $("#status").empty();
+      setStatus("Done loading <a href=\"../autogen/nodemapper.js\">" +
+          "nodemapper.js</a>");
+      nodemapperReady();
+    });
+  }
 });
+
+function toggleDebug(newDebug) {
+  var oldUseDebug = useDebug;
+  useDebug = newDebug != undefined ? newDebug : !useDebug;
+  if (oldUseDebug != newDebug)
+    window.location = getPermalink();
+  var textbox;
+  if (useDebug)
+    textbox = "Leave debug mode";
+  else
+    textbox = "Enter debug mode";
+  $("#settings_debug").val(textbox);
+}
+
+function getPermalink() {
+  var url = window.location.href;
+  url = url.indexOf("?") == -1 ? url : url.substring(0, url.indexOf("?"));
+  var params = {};
+  if (useDebug)
+    params.debug = 1;
+  if (effects != -1)
+    params.effects=effects;
+  if ($("#tosgn_input").val() != "")
+    params.q = $("#tosgn_input").val();
+  if ($("#fromsgn_input").val() != "" && !params.q)
+    params.sgn = $("#fromsgn_input").val();
+  var paramsCompile = $.param(params);
+  if (paramsCompile.length > 0)
+    url+="?"+paramsCompile;
+  return url;
+}
+
+function getQueryString() {
+  var q = new Object();
+  $.each(location.search.substring(1).split("&"), function(i) {
+    if (this == "")
+      return false;
+    var pair = this.split("=");
+    q[pair[0]] = pair[1];
+  });
+  return q;
+}
+
+function nodemapperReady() {
+  $("#tosgn_form").submit(function() {
+    convertToSgn();
+    return false;
+  });
+  $("#fromsgn_form").submit(function() {
+    convertFromSgn();
+    return false;
+  });
+  if (queryString.q) {
+    convertToSgn();
+  } else if (queryString.sgn) {
+    convertFromSgn();
+  }
+  $("#settings_permalink").click(function() {
+    var url = getPermalink();
+    $("#permalink_output").hide(effects).html("<a href='" + url + "'>" + url +
+        "</a>").show(effects);
+  });
+}
 
 function parseHtml(data, regex) {
   var sites = [];
@@ -68,8 +163,8 @@ function findSites(data) {
   var sites = [];
   $.each(regex, function() {
     sites = parseHtml(data, this);
-    //we got results, so we use return false, to break out of the $.each loop
-    if(sites.length > 0)
+    //we got results, so we  return false, to break out of the $.each loop
+    if (sites.length > 0)
       return false;
   });
   if (sites.length == 0)
@@ -78,25 +173,26 @@ function findSites(data) {
 }
 
 function setStatus(text) {
-  $("#status").append("<div>" + text + "</div>");
+  $("<div>" + text + "</div>").hide().appendTo("#status").show(effects);
 }
 
 function convertToSgn() {
-  var input = $("#tosgn").val();
+  var input = $("#tosgn_input").val();
   var output = nodemapper.urlToGraphNode(input);
   if (output == input) {
-    $("#tosgn_output").html("No known conversion for " + output);
-    return false;
+    $("#tosgn_output").hide(effects).html("No known conversion for " +
+        output).show(effects);
+  }else {
+    $("#tosgn_output").hide(effects).html("Converted to: <b>" + output +
+        "</b> (from " + input + ")").show(effects, function() {
+      $("#fromsgn_input").val(output);
+      convertFromSgn();
+    });
   }
-  $("#tosgn_output").html("Converted to: <b>" + output + "</b> (from " +
-      input + ")");
-  $("#fromsgn").val(output);
-  convertFromSgn();
-  return false;
 }
 
 function convertFromSgn() {
-  var input = $("#fromsgn").val();
+  var input = $("#fromsgn_input").val();
   var types = ["profile", "content", "atom", "rss", "blog", "openid", "foaf",
       "addfriend"];
   var output = "";
@@ -112,6 +208,5 @@ function convertFromSgn() {
   if (output.length == 0) {
     output = "<i>No known mappings to HTTP.</i>";
   }
-  $("#fromsgn_output").html(output);
-  return false;
+  $("#fromsgn_output").hide(effects).html(output).show(effects);
 }
